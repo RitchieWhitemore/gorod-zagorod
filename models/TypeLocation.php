e71d5156807b61081e60bc98e0ed68f2aa52a246
RitchieWhitemore <RichiB@yandex.ru>
2019-03-22 15:41:13 +0300

    Добавил модель TypeLocation

---
+++ models/TypeLocation.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "type_location".
+ *
+ * @property int $id
+ * @property string $name
+ *
+ * @property Location[] $locations
+ */
+class TypeLocation extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'type_location';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getLocations()
+    {
+        return $this->hasMany(Location::className(), ['type_location_id' => 'id']);
+    }
+}
