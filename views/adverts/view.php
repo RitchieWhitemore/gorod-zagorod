d167be179395555ec523666e276c068dec24e30e
RitchieWhitemore <RichiB@yandex.ru>
2019-04-10 13:40:17 +0300

    Вывел характеристики на странице объявления

--- views/adverts/view.php
+++ views/adverts/view.php
@@ -6,25 +6,25 @@
  */
 
 $this->params['h1'] = $model->fullAddress;
-$this->title = 'Объявление: ' .  $model->fullAddress;
+$this->title = 'Объявление: ' . $model->fullAddress;
 ?>
 
-<?= \app\widgets\Filter::widget()?>
+<?= \app\widgets\Filter::widget() ?>
 <section class="page-property container">
     <div class="page-property__content-wrapper">
         <div class="page-property__content">
             <div class="page-property__image-gallery image-gallery">
                 <div class="image-gallery__main-image">
                     <img src="<?= Yii::$app->thumbnail->url($model->mainImagePath, [
-                        'thumbnail' => [
-                            'width' => 855,
+                        'thumbnail'   => [
+                            'width'  => 855,
                             'height' => 570,
                         ],
                         'placeholder' => [
-                            'width' => 855,
+                            'width'  => 855,
                             'height' => 570
                         ]
-                    ])?>" >
+                    ]) ?>">
                     <span class="image-gallery__label image-gallery__label--buy"><?= $model->typeAdvert->name_type_offer ?></span>
                     <div class="image-gallery__control">
                         <button type="button" class="image-gallery__button image-gallery__button--prev"></button>
@@ -40,43 +40,37 @@ $this->title = 'Объявление: ' .  $model->fullAddress;
                     <li class="image-gallery__item"><img src="images/property3.jpg"></li>
                 </ul>
             </div>
-            <h2 class="page-property__title"><?= $model->property->name?>, <?= $model->area ?> м<sup>2</sup></h2>
+            <h2 class="page-property__title"><?= $model->property->name ?>, <?= $model->area ?> м<sup>2</sup></h2>
             <div class="page-property__row">
-                <p class="page-property__address"><?= $model->fullAddress?></p>
-                <span class="page-property__price"><?= $model->price?> Р</span>
+                <p class="page-property__address"><?= $model->fullAddress ?></p>
+                <span class="page-property__price"><?= $model->price ?> Р</span>
             </div>
+
+            <?php
+            $characteristics = $model->getCharacteristicValues()->all();
+            ?>
 
             <table class="page-property__table">
                 <thead>
                 <tr>
-                    <th>
-                        Общая
-                    </th>
-                    <th>
-                        Жилая
-                    </th>
-                    <th>
-                        Кухня
-                    </th>
-                    <th>
-                        Этаж
-                    </th>
+                    <?php foreach ($characteristics as $characteristic) : ?>
+
+                        <th>
+                            <?=$characteristic->characteristic->name?>
+                        </th>
+
+                    <?php endforeach; ?>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <td>
-                        99 м2
-                    </td>
-                    <td>
-                        52 м2
-                    </td>
-                    <td>
-                        15 м2
-                    </td>
-                    <td>
-                        5 из 6
-                    </td>
+                    <?php foreach ($characteristics as $characteristic) : ?>
+
+                        <td>
+                            <?=$characteristic->value?>
+                        </td>
+
+                    <?php endforeach; ?>
                 </tr>
                 </tbody>
             </table>
@@ -84,7 +78,7 @@ $this->title = 'Объявление: ' .  $model->fullAddress;
         </div>
 
         <div class="page-property__map">
-            <iframe src="<?= $model->linkMapForWidget?>" frameborder="0"
+            <iframe src="<?= $model->linkMapForWidget ?>" frameborder="0"
                     allowfullscreen="true"></iframe>
         </div>
         <div class="page-property__content">
@@ -93,8 +87,8 @@ $this->title = 'Объявление: ' .  $model->fullAddress;
 
     </div>
     <?= \app\widgets\SimilarAdverts::widget([
-            'model' => $model,
-    ])?>
+        'model' => $model,
+    ]) ?>
     <aside class="page-property__aside">
         <?= \app\widgets\Locations::widget() ?>
     </aside>
